be4d29fad21b6e0c80d22bd4e7c4c818d525f1e2
Mateus Martinez Rosa <mateusrttim@hotmail.com>
2019-07-06 12:33:53 -0300

    template admin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -7,16 +7,16 @@ class Page{
 
     private $tpl;
     private $options = [];
-    private $defauts =[
+    private $defaults =[
         "data"=>[]
     ];
     
-	public function __construct($opts = array()){
+	public function __construct($opts = array(), $tpl_dir = "/views/"){
 
-        $this->options = array_merge($this->defauts, $opts);
+        $this->options = array_merge($this->defaults, $opts);
 
         $config = array(
-            "tpl_dir"       =>$_SERVER["DOCUMENT_ROOT"]. "/views/",
+            "tpl_dir"       =>$_SERVER["DOCUMENT_ROOT"]. $tpl_dir,
             "cache_dir"     =>$_SERVER["DOCUMENT_ROOT"]. "/views-cache/",
             "debug"         => false 
         );
@@ -28,6 +28,8 @@ class Page{
         $this->setData($this->options["data"]);
 
         $this->tpl->draw("header");
+
+     
 
     }
 
